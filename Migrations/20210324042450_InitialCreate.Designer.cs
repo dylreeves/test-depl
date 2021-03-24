8ab9554f67e89794e0e98cc3d13f54accd43b800
Ubuntu <test-admin@staging-vm.gczdoc21irgudihf40qeupvaed.bx.internal.cloudapp.net>
2021-03-24 04:38:28 +0000

    Add Done Field

--- Migrations/20210324042450_InitialCreate.Designer.cs
+++ Migrations/20210324042450_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetCoreSqlDb.Migrations
 {
     [DbContext(typeof(MyDatabaseContext))]
-    [Migration("20210324041317_InitialCreate")]
+    [Migration("20210324042450_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
